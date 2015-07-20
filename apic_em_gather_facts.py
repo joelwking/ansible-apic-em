e9124bf7f7e1274ba8b1cd4b6d277c0338c4bec8
Joel W. King <joel.king@wwt.com>
2015-07-20 10:52:46 -0400

    1.1 - added documentation and error checking

--- apic_em_gather_facts.py
+++ apic_em_gather_facts.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
 """
-     Copyright (c) 2014 World Wide Technology, Inc. 
+     Copyright (c) 2015 World Wide Technology, Inc. 
      All rights reserved. 
 
      Revision history:
      17 July 2015  |  1.0 - initial release
+     20 July 2015  |  1.1 - added documentation and error checking
  
 """
 
@@ -13,7 +14,7 @@ DOCUMENTATION = '''
 ---
 module: apic_em_gather_facts
 author: Joel W. King, World Wide Technology
-version_added: "1.0"
+version_added: "1.1"
 short_description: query the APIC-EM controller for an inventory of network devices
 description:
     - This module issues a query of an APIC-EM controller to obtain the addresss of network devices in the inventory
@@ -45,35 +46,60 @@ options:
             - Login password
         required: true
 
+    debug:
+        description:
+            - debug switch
+        required: false
+
 
 '''
 
 EXAMPLES = '''
 
- 
+ $ ./hacking/test-module -m /home/administrator/ansible/lib/ansible/modules/extras/network/apic_em_gather_facts.py 
+                         -a "username=bob password=xxxxxx host=10.255.40.125"             
+***********************************
+PARSED OUTPUT
+{
+    "ansible_facts": {
+        "mgmtIp": [
+            "10.255.138.120",
+            "10.255.138.121",
+            "10.255.138.123",
+            "10.255.138.122"
+        ]
+    },
+    "changed": false
+}
+
 
 '''
+
 
 import sys
 import time
 import json
 import requests
 
+
+
 # ---------------------------------------------------------------------------
 # APIC-EM Connection Class
 # ---------------------------------------------------------------------------
 class Connection(object):
     """
-      Connection class for Python to APIC-EM controller REST Calls, CA2 distribution,
-      to test this class use the following from your main program or interpreter:
+      Connection class for Python to APIC-EM controller REST Calls, CA2 distribution.
+
+      Examples of how to test interactively from Python.
 
          k = Connection()
-         status, msg = k.aaaLogin("10.255.40.125", 'kingjoe', 'foobar')
+         status, msg = k.aaaLogin("10.255.40.125", 'bob', 'xxxxxx')
          status, msg = k.genericGET("/api/v1/network-device/count")
+         status, msg = k.genericGET("/api/v1/reachability-info")
    
     """
     def __init__(self):                               
-        self.version = "1.0"
+        self.api_version = "1.0"
         self.transport = "https://"
         self.controllername = "192.0.2.1"
         self.username = "admin"
@@ -82,31 +108,44 @@ class Connection(object):
         self.serviceTicket = {"X-Auth-Token: <your-ticket>"}
 
         return
+#
+#
+#
     def aaaLogin(self, controllername, username, password):
         """ 
+        Logon the controller, need to pass the userid and password, and in return we get a token.
+        Do not know how log the token is valid. An example in cURL is
+
          $ curl -k -H "Content-Type: application/json" 
                    -X POST -d '{"username": "<username>", "password": "<password>"}' 
                    https://<controller-ip>/api/v1/ticket
         """
         self.controllername = controllername
+        self.username = username
+        self.password = password
+
         URL = "%s%s/api/v1/ticket" % (self.transport, self.controllername)
-        DATA = {"username": username, "password": password}
+        DATA = {"username": self.username, "password": self.password}
         try:
             r = requests.post(URL, data=json.dumps(DATA), headers=self.HEADER, verify=False)
         except requests.ConnectionError as e: 
-            return (False, e)
+            return (False, str(e))
         else:
             content = json.loads(r.content)
             try:
-                self.version = content["version"]
+                self.api_version = content["version"]
                 self.serviceTicket = {"X-Auth-Token": content["response"]["serviceTicket"]}
             except KeyError:
-                return (False, "KeyError")
+                return (False, "Login failure")
             else:
                 return (r.status_code, content)
-
+#
+#
+#
     def genericGET(self, URL):
         """
+         Issue an HTTP GET base on the URL passed as an argument and example in cURL is:
+  
          $ curl -k -H "X-Auth-Token: <your-ticket>" 
                    https://<controller-ip>/api/v1/network-device/count
         """
@@ -119,79 +158,63 @@ class Connection(object):
         return (r.status_code, content['response'])
 
 
+
 # ---------------------------------------------------------------------------
-# PROCESS
+# get_discovered_devices
 # ---------------------------------------------------------------------------
 
-def process(cntrl):
-    """ We have all are variables and parameters set in the object, attempt to 
-        login and post the data to the APIC
+def get_discovered_devices(cntrl):
+    """ Query controller for device level reachability information for all devices.
+        Returned is a list of dictionaries describing the network device, for example:
+
+        [{u'discoveryId': u'938fb083-be67-4a72-9306-10aacffe26c8',
+          u'discoveryStartTime': u'2015-07-17 19:11:16.932452+00',
+          u'enablePassword': u'*****',
+          u'id': u'035cde8e-bf12-400c-b510-adc23bafcf73',
+          u'mgmtIp': u'10.255.138.120',
+          u'password': u'*****',
+          u'protocolList': u'SSH,TELNET,HTTPS,HTTP',
+          u'protocolUsed': u'SSH',
+          u'reachabilityStatus': u'Discovered',
+          u'userName': u'admin'}]
+
+       Currently only returning the management IP address, but ideally we should return the
+       passwords, so we don't have to maintain them in Ansible. 
+
     """
-
-    if cntrl.aaaLogin() != 200:
-        return (1, "Unable to login to controller")
-
-    rc = cntrl.genericGET()
-    if rc == 200:
-        return (0, format_content(cntrl.get_content()))
-    else:
-        return (1, "%s: %s" % (rc, httplib.responses[rc]))
-
-
-# ---------------------------------------------------------------------------
-# FORMAT_CONTENT
-# ---------------------------------------------------------------------------
-def format_content(content):
-    """ formats the content into an Ansible fact 
-
-    from ACI a class query returns:
-   
-     dict         list       dict        dict
-    "imdata"    [ aaaUser: attributes: elements, ...]
-    "totalcount"
+    element = { 'mgmtIp' : [] }
+    result = { 'ansible_facts': {} }                      
     
-    Here is an example of the core setup module
-
-    $ ./bin/ansible 10.255.40.207 -m setup --ask-pass -a 'filter=ansible_all_ipv4_addresses'
-    SSH password:
-    10.255.40.207 | success >> {
-        "ansible_facts": {
-            "ansible_all_ipv4_addresses": [
-                "10.255.40.207"
-            ]
-        },
-        "changed": false
-    }
-
-    """
-    element = {}                                           # dictionary to hold the class
-    result = { 'ansible_facts': {} }                       # the result is a dictionary with one element called 'ansible_facts'
-    content = json.loads(content)["imdata"]                # remove the IMDATA wrapper
-    for item in content:                                   # content is a *list* of one or more elements returned for the class query
-        d_item = dict(item)
-        aci_class = d_item.keys()[0]                       # get the name of the class we queried
+    status, response = cntrl.genericGET("/api/v1/reachability-info")
+    for device in response:
         try:
-            element[aci_class]
+            if device["reachabilityStatus"] == "Discovered":
+                element['mgmtIp'].append(device['mgmtIp'])
         except KeyError:
-            element[aci_class] = []                        # each returned MO is a list element
+            pass
 
-        attributes = d_item[aci_class]["attributes"]
-        element[aci_class].append(attributes)              # append the MO to our class dictionary
-
+    logoff()
     result["ansible_facts"] = element
-    return result
-        
+    return status, result
+
+
+# ---------------------------------------------------------------------------
+# logoff
+# ---------------------------------------------------------------------------
+
+def logoff():
+    """ Need documentation if logoff is implemented or necessary """
+    return
+
 
 # ---------------------------------------------------------------------------
 # MAIN
 # ---------------------------------------------------------------------------
 
 def main():
-
+    "   "
     module = AnsibleModule(
         argument_spec = dict(
-            queryfilter = dict(required=False),
-            URI = dict(required=True),
             host = dict(required=True),
             username = dict(required=True),
             password  = dict(required=True),
@@ -201,23 +224,17 @@ def main():
         add_file_common_args=True
     )
     
-    cntrl = AnsibleACI.Connection()
-    cntrl.setcontrollerIP(module.params["host"])
-    cntrl.setUsername(module.params["username"])                               
-    cntrl.setPassword(module.params["password"])
-
-    cntrl.setgeneric_URL("%s://%s" + module.params["URI"] + queryfilter)
-                                  
-    code, response = process(cntrl)
-    cntrl.aaaLogout()
-
-    if code == 1:
-        module.fail_json(msg=response)
+    cntrl = Connection()
+    connected, msg = cntrl.aaaLogin(module.params["host"], module.params["username"], module.params["password"])
+    if connected:
+        code, response = get_discovered_devices(cntrl)
+        if code == 200:
+            module.exit_json(**response)
+        else:
+            module.fail_json(msg="status_code= %s" % code)
     else:
-        module.exit_json(**response)
-  
-    return code
-
+        module.fail_json(msg=msg)
+    
 
 from ansible.module_utils.basic import *
 main()
